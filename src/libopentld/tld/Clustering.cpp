a843d08869896728d52e7b2435636df484542976
忆夕羊 <sunzhu_sz@163.com>
2016-05-06 22:27:07 +0800

    修改

--- src/libopentld/tld/Clustering.cpp
+++ src/libopentld/tld/Clustering.cpp
@@ -87,7 +87,7 @@ void Clustering::calcDistances(float *distances)
 {
     float *distances_tmp = distances;
 
-    std::ector<int> confidentIndices = *detectionResult->confidentIndices;
+    std::vector<int> confidentIndices = *detectionResult->confidentIndices;
 
     size_t indices_size = confidentIndices.size();
 
